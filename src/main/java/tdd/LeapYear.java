6d43158e79dfa640dd0b7a7b9a67edb010878d22
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-06-12 13:20:41 +0530

    Refactor - Extract logic to separate functions.

--- src/main/java/tdd/LeapYear.java
+++ src/main/java/tdd/LeapYear.java
@@ -2,18 +2,29 @@ package tdd;
 
 public class LeapYear {
     public static Boolean isLeapYear(int leapYear) {
-        if (leapYear % 4000 == 0) {
-            return false;
-        }
-        if (leapYear % 4 == 0) {
-            if (leapYear % 100 == 0) {
-                if (leapYear % 400 == 0) {
-                    return true;
-                }
-                return false;
+        if (divisibleBy4000(leapYear)) return false;
+        if (divisibleBy4(leapYear)) {
+            if (divisibleBy100(leapYear)) {
+                return !divisibleByOnly100andNotDivisible400(leapYear);
             }
             return true;
         }
         return false;
+    }
+
+    private static boolean divisibleBy4(int leapYear) {
+        return leapYear % 4 == 0;
+    }
+
+    private static boolean divisibleBy100(int leapYear) {
+        return leapYear % 100 == 0;
+    }
+
+    private static boolean divisibleByOnly100andNotDivisible400(int leapYear) {
+        return leapYear % 100 == 0 && leapYear % 400 != 0;
+    }
+
+    private static boolean divisibleBy4000(int leapYear) {
+        return leapYear % 4000 == 0;
     }
 }
